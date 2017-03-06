d6782e2cf97266771429dd0e0c7a8421da07963c
ISAAC300 <isaac.rec@gmail.com>
2017-03-06 17:42:16 -0400

    Update main.cpp

--- jni/main.cpp
+++ jni/main.cpp
@@ -2,15 +2,25 @@
 #include "utils/StringUtils.h"
 #include "utils/FileUtils.h"
 #include "interpreter/Interpreter.h"
+#include "interpreter/Variable.h"
 
 #define print(str) std::cout << str << std::endl;
 
-int main(){
-	Interpreter* interpreter = new Interpreter("bool isNull = true;");
-	interpreter->setListener(new Interpreter::Listener());
-	interpreter->append("log('10');");
-	interpreter->append("file_create('10');");
-	interpreter->run();
+class Listener : public Interpreter::Listener{
+	public:
+	virtual void onRun(Interpreter*parent,int line,std::string const& data);
+};
+
+void Listener::onRun(Interpreter*parent,int line,std::string const& data){
 	
+}
+
+int main(){
+	print((new Variable("bool test = 0;"))->getType_());
+	Interpreter* interpreter = new Interpreter("bool isNull = true;");
+	interpreter->addListener(new Listener());
+	interpreter->append("log('10');");
+	interpreter->append("file_create(sdcard/atatxt.txt);");
+	interpreter->run();
 	return 0;
 }
